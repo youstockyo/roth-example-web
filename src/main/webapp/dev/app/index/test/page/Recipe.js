8fe7e5195e6152b2a2f2f39fd61439f9e2c6e1ce
youstockyo <cjeustaquio@gmail.com>
2016-11-03 16:04:43 -0700

    adding recipe page, service, styling

---
+++ src/main/webapp/dev/app/index/test/page/Recipe.js
@@ -0,0 +1,31 @@
+
+var View = function(data)
+{
+	
+};
+
+View.config =
+{
+  
+};
+
+View.prototype.init = function()
+{
+	
+};
+
+View.prototype.ready = function()
+{
+	
+};
+
+View.prototype.change = function(changeParam)
+{
+	
+};
+
+View.prototype.visible = function()
+{
+	
+};
+
